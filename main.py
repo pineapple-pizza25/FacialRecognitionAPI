befa3aba5a3d3b0363163767be43c8d9c745bcda
pineapple_pizza25 <st10066487@vcconnect.edu.za>
2024-09-06 00:21:53 +0200

    commit 3 - uses JSON

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File, Form, UploadFile
+from fastapi.responses import JSONResponse, FileResponse
+from pydantic import BaseModel
+import numpy as np
 from fastapi.middleware.cors import CORSMiddleware
 from deepface import DeepFace
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo import MongoClient
 import bson.binary
+import PIL
+from PIL import Image, ImageDraw, ImageFont
+import io
+import numpy as np
+import tensorflow as tf
+from tempfile import NamedTemporaryFile
+import logging
+from deepface.modules import verification
+from io import BytesIO
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
+
+UPLOAD_DIR = "uploaded_images"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+origins = ["http://localhost:3000"]
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client['facial_recognition']
@@ -26,15 +46,14 @@ def getImage():
     return {"Welcome"}
 
 @app.post("/storeface")
-def storeImageEmbedding(img_path: str = "face_image.jpg"):
-    #img = functions.preprocess_face(img_path, target_size=(224, 224))
-
-    if not os.path.exists(img_path):
-        raise HTTPException(status_code=404, detail="Image file not found")
+async def storeImageEmbedding(file:UploadFile = File(...)):
     
+    raw_file = await file.read()
+    image = file_to_image(raw_file)
+    npArray = pil_to_numpy(image)
 
     try:
-        embedding = DeepFace.represent(img_path, model_name = "Facenet")
+        embedding = DeepFace.represent(npArray)
 
         collection.insert_one({
             "image_name": "image1", 
@@ -48,4 +67,64 @@ def storeImageEmbedding(img_path: str = "face_image.jpg"):
     
     except Exception as e:
         raise HTTPException(status_code=500, detail="There was an error:"+str(e))
+
+
+@app.post("/facialrecognition")
+async def getFacialRecognition(file:UploadFile = File(...)):
+
+    raw_file = await file.read()
+    image = file_to_image(raw_file)
+    npArray = pil_to_numpy(image)
+
+    image1_path = "C:/Users/snmis/OneDrive/Pictures/Camera Roll/WIN_20240903_14_52_19_Pro.jpg"
+
+    try:
+        if DeepFace.verify(npArray, image1_path,threshold=0.6)['verified']:
+            return {"message":"This is the same dude"}
+        else: 
+            return{"message":"this is not the same dude"}
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Something went wrong: {str(e)}")
+
+
+
+
+@app.post("/verifyface")
+async def postVerifyFace(file:UploadFile = File(...)):
+
+    raw_file = await file.read()
+    image = file_to_image(raw_file)
+    npArray = pil_to_numpy(image)
+
+    img1_representation = DeepFace.represent(npArray)
+    img2_representation = DeepFace.represent(npArray2)
+
+    distance = verification.find_euclidean_distance(img1_representation, img2_representation)
+    threshold = verification.find_threshold('VGG-face', 'euclidean')
+
+    if distance <= threshold:
+        return {"message":"This is the same dude"}
+    else:
+        return {"message":"This is not the same dude"}
+    
+
+
+
+def file_to_image(file):
+  image = Image.open(BytesIO(file)).convert("RGB")
+  return image
+
+
+
+def pil_to_numpy(img: Image.Image) -> np.ndarray:
+    return np.array(img)
+
+
+
+
+def load_image_into_numpy_array(data):
+    return np.array(Image.open(BytesIO(data)))
+
+
 
